e043fd9dfc0319b4f8e87e9c324a883a5705c4fa
Charlie Xu <charliex@slac.stanford.edu>
2018-06-27 15:45:57 -0700

    adding BAT monitor matlab wrapper description file

--- Model/PhaseCavity2856_freq_calc.m
+++ Model/PhaseCavity2856_freq_calc.m
@@ -17,5 +17,5 @@ if2adc_r = Fif2600/Fadc2600
 Fadc2856 = 357e6    % ADC clk for 2856MHz board
 Fif2856  = Fadc2856 * if2adc_r  % Calculating IF frequency base on ADC clk and ratio
 Fif2856_og = 85e6;
-Flo2856  = 2856e6 - Fif2856_og
-Fcav2856 = Flo2856 + Fif2856
+Flo2856  = 2856e6 - Fif2856_og;
+Fcav2856 = Flo2856 + Fif2856 % New cavity frequency
