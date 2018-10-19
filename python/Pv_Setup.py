511a228b2544ae42407b4ce885b245e3708e68eb
Charlie Xu <charliex@slac.stanford.edu>
2018-10-19 16:26:11 -0700

    fixing PV dict

--- python/Pv_Setup.py
+++ python/Pv_Setup.py
@@ -134,7 +134,7 @@ def trigger_pv_setup(Cav_Sys, evr='', evr_index=1):
 
 	for i in range(evr_index):
 		if i == 0:
-			Cav_Sys.EVR_PV_Trig_Ena[i] = evr + 'TRIG0:EVENT' + Str_Out
+			Cav_Sys.EVR_PV_Trig_Ena[i] = evr + ':TRIG0:EVENT' + Str_Out
 			Cav_Sys.EVR_PV_Trig_Eventcode[i] = evr + 'TRIG0:EVENT' + Str_Eventcode
 			Cav_Sys.EVR_PV_Trig_Eventcode_Ena[i] = evr + 'TRIG0:EVENT' + Str_Event_Enable
 		else:
